42c5463b92e90b479b65113287bae5237c4d0dda
EelenZ <3143157934@qq.com>
2020-12-13 21:59:30 +0800

    commit

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,14 @@
 use std::error::Error;
 
 use combine::parser;
-///KeyWord table
-type UResult<U, V> = std::result::Result<(U, V), Box<dyn Error + 'static>>;
-
+//KeyWord table
 const kwtb: [&str; 32] = ["int", "char", "double", "float", "short", "long", "auto", "break",
                         "case", "const", "continue", "default", "do", "else", "enum", "extern",
                         "for", "goto", "if", "register", "return", "signed", "sizeof", "static",
                         "struct", "switch", "typedef", "unsigned", "union", "void", "volatile", "while"
 ];
+
+
 
 pub fn isKeyWord(s: &str) -> bool {
      for item in kwtb.into_iter() {
@@ -29,12 +29,24 @@ pub fn isIdentifier(c: char) -> bool{
      false
 } 
 
-pub fn recognize(s: &str) {
+pub fn recognize(s: &str) -> Option<(usize, usize)>{
      let mut ops: usize = 0;
      let mut opnds: usize = 0;
-     
+     let mut i: usize = 0;
      let mut container = String::new();
      let cs: Vec<char> = s.chars().collect();
+     while i < cs.len() {
+          if isIdentifier(cs[i]) {
+               container.push(cs[i]);
+          }
+          else {
+               ops += 1;
+               if !isKeyWord(container.as_str()) {
+                    opnds += 1;
+               }
+               container.clear();
+          }
+     }
      todo!()
 }
 
