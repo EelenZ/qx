b05882a19ec3564eb697f07201bd1c6f4bc70c98
EelenZ <3143157934@qq.com>
2020-12-16 18:50:56 +0800

    commit

--- src/util/format.rs
+++ src/util/format.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use async_std::{path::PathBuf,
                 path::Path,
+                fs::OpenOptions,
                 fs::File,
                 fs::read_dir,
                 io::{BufReader, BufWriter, prelude::BufReadExt},
@@ -10,26 +11,57 @@ use futures::StreamExt;
 
 type Result<T> = std::result::Result<T, Box<dyn Error + 'static>>;
 
-pub async fn read_project(path: PathBuf) -> Result<()> {
-    let mut project_root = read_dir(path).await?;
-    let mut dir_list: Vec<PathBuf> = Vec::new();
+pub async fn read_project(root: PathBuf) -> Result<()> {
+    let mut path_list: Vec<PathBuf> = Vec::new();
     let mut file_list: Vec<PathBuf> = Vec::new();
-    while let Some(res) = project_root.next().await {
-        let path = res?.path();
-        if path.is_dir().await {
-            dir_list.push(path);
-        }
-        else if path.ends_with(".rs"){
+    path_list.push(root);
+
+    while let Some(path) = path_list.pop() {
+        if path.is_file().await {
             file_list.push(path);
         }
-    }
-    
+        else if path.is_dir().await {
+            let mut dir = read_dir(path).await?;
+            while let Some(res) = dir.next().await {
+                let path = res?.path();
+                if path.is_dir().await {
+                    path_list.push(path);
+                }
+                else if path.is_file().await{
+                    file_list.push(path);
+                }
+            }
+        }    
+    }  
     Ok(())
 }
 
-pub async fn list_file(path: PathBuf, dir_list: &mut Vec<PathBuf>, file_list: &mut Vec<PathBuf>) {
+pub async fn format( file_list: &Vec<PathBuf>)-> Result<()>{
+    for file_path in file_list {
+        let mut file = BufReader::new(OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(file_path)
+            .await?
+        );
+        format_file(&mut file).await;
+    }
+    Ok(())
+}
+
+pub async fn format_file(file: &mut BufReader<File>) -> Result<()>{
+    while let Some(line) = file.lines().next().await {
+        let line = line?;
+        format_line(line).await;
+    }
+    Ok(())
+}
+
+pub async fn format_line(line: String) -> Result<()> {
+    let line = line.trim();
     
-    todo!()
+    Ok(())
 }
 
 pub async fn format_code(path: PathBuf) -> Result<()>{
